038b4ad4eedfc0d0911b4f6329c9c1a1e0180bfd
Carlos <carlosoftwar@gmail.com>
2021-04-01 22:25:53 -0300

    Update : Adicionando Rotas CRUD

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,6 +1,41 @@
+const Agendamento = require('../models/Agendamento');
+
+
 module.exports = app => {
-    app.get('/agendamentos', (req, res) => {
-        res.send("Servidor OK");
+
+    app.get('/agenda', (req, res) => {
+
+        Agendamento.exibir(res);
+
     });    
-    //app.post()
+    app.post('/agenda', (req,res) => {
+
+        var agendamento = req.body;
+        Agendamento.inserir(agendamento, res);
+    
+    })
+    
+    app.get('/agenda/:id', (req,res) => {
+
+        const id = parseInt(req.params.id);
+        Agendamento.exibirUser(id, res);
+
+    })
+
+    app.delete('/agenda/:id', (req,res) => {
+
+        const id = parseInt(req.params.id);
+        Agendamento.excluir(id, res);
+    
+    })
+
+    app.put('/agenda/:id', (req,res) => {
+        const id = parseInt(req.params.id);
+        const data = req.body;
+
+        Agendamento.atualizar(id,data, res);
+    })
+
+
 };
+
